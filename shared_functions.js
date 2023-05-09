b477af25ad5d325da7627f334596ced0226357d9
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2023-05-09 09:58:13 +0800

    Update shared_functions.js

--- shared_functions.js
+++ shared_functions.js
@@ -2,9 +2,9 @@ function navigateToJisho(searchText) {
     var jishoURL = "https://jisho.org/search/" + searchText.trim();
     // If this is a single kanji character, append #kanji to the URL
     // to navigate directly to the kanji page
-    if (/^[\u4e00-\u9faf]$/.test(searchText)) {
-        jishoURL = jishoURL + "%23kanji";
-    }
+//     if (/^[\u4e00-\u9faf]$/.test(searchText)) {
+//         jishoURL = jishoURL + "%23kanji";
+//     }
     chrome.tabs.create({
         url: jishoURL
     });
